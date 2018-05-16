9238300c0104989e29ac31d0a2d9956c889db772
esBeee <sebastianbitzer@posteo.de>
2018-05-16 21:59:19 +0700

    Refactor repository structure

--- tests/test_assert_allclose.py
+++ tests/test_assert_allclose.py
@@ -1,6 +1,6 @@
 import unittest
 import torch
-from assertions import assert_allclose
+import torch_testing as tt
 
 
 class TestAssertAllclose(unittest.TestCase):
@@ -8,26 +8,26 @@ class TestAssertAllclose(unittest.TestCase):
     def test_with_allclose_tensors(self):
         a = torch.tensor([1, 2, 23.65799, 0])
         b = torch.tensor([1, 2, 23.657989, 0])
-        assert_allclose(a, b)
+        tt.assert_allclose(a, b)
 
     def test_with_not_allclose_tensors(self):
         a = torch.tensor([1, 2, 23.6579, 0])
         b = torch.tensor([1, 2, 23.65789, 0])
 
         with self.assertRaisesRegex(AssertionError, 'Not equal to tolerance'):
-            assert_allclose(a, b)
+            tt.assert_allclose(a, b)
 
     def test_atol_param_with_allclose_tensors(self):
         a = torch.tensor([0, 0, 0])
         b = torch.tensor([-1, 0, 1])
-        assert_allclose(a, b, atol=1, rtol=0)
+        tt.assert_allclose(a, b, atol=1, rtol=0)
 
     def test_atol_param_with_not_allclose_tensors(self):
         a = torch.tensor([0])
         b = torch.tensor([-1.001])
 
         with self.assertRaisesRegex(AssertionError, 'Not equal to tolerance'):
-            assert_allclose(a, b, atol=1, rtol=0)
+            tt.assert_allclose(a, b, atol=1, rtol=0)
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_assert_equal.py
+++ tests/test_assert_equal.py
@@ -1,6 +1,6 @@
 import unittest
 import torch
-from assertions import assert_equal
+import torch_testing as tt
 
 
 class TestAssertEqual(unittest.TestCase):
@@ -8,17 +8,17 @@ class TestAssertEqual(unittest.TestCase):
     def test_with_empty_tensors(self):
         a = torch.tensor([])
         b = torch.tensor([])
-        assert_equal(a, b)
+        tt.assert_equal(a, b)
 
     def test_with_equal_one_value_tensors(self):
         a = torch.tensor([23.65])
         b = torch.tensor([23.65])
-        assert_equal(a, b)
+        tt.assert_equal(a, b)
 
     def test_with_equal_1_dimensional_tensors(self):
         a = torch.tensor([23.65, 8.2])
         b = torch.tensor([23.65, 8.2])
-        assert_equal(a, b)
+        tt.assert_equal(a, b)
 
     def test_with_equal_2_dimensional_tensors(self):
         a = torch.tensor([
@@ -29,26 +29,26 @@ class TestAssertEqual(unittest.TestCase):
             [23.65, 9.3, 5.2],
             [8.2, 1.1, 9],
         ])
-        assert_equal(a, b)
+        tt.assert_equal(a, b)
 
     def test_with_equal_tensors_of_different_type(self):
         a = torch.tensor([4], dtype=torch.int32)
         b = torch.tensor([4.0], dtype=torch.float16)
-        assert_equal(a, b)
+        tt.assert_equal(a, b)
 
     def test_with_unequal_one_value_tensors(self):
         a = torch.tensor([23.65])
         b = torch.tensor([23.66])
 
         with self.assertRaisesRegex(AssertionError, 'Arrays are not equal'):
-            assert_equal(a, b)
+            tt.assert_equal(a, b)
 
     def test_with_tensors_of_different_dimension(self):
         a = torch.tensor([23.65, 1])
         b = torch.tensor([23.66])
 
         with self.assertRaisesRegex(AssertionError, 'Arrays are not equal'):
-            assert_equal(a, b)
+            tt.assert_equal(a, b)
 
     def test_with_unequal_2_dimensional_tensors(self):
         a = torch.tensor([
@@ -61,7 +61,7 @@ class TestAssertEqual(unittest.TestCase):
         ])
 
         with self.assertRaisesRegex(AssertionError, 'Arrays are not equal'):
-            assert_equal(a, b)
+            tt.assert_equal(a, b)
 
 if __name__ == '__main__':
     unittest.main()
