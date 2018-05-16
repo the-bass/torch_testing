5a13486740125f764de62d6f2a56d8f420525b50
esBeee <sebastianbitzer@posteo.de>
2018-05-16 21:59:40 +0700

    Add setup.py for building the package using pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='torch_testing',
+      version='0.0.1',
+      description='A collection of assertion methods to compare PyTorch Tensors',
+      url='https://github.com/the-bass/torch_testing',
+      author='The Bass',
+      author_email='dont@wanna.tell',
+      license='MIT',
+      packages=['torch_testing'],
+      zip_safe=False)
